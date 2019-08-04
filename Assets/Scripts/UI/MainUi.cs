fc67b206567919e21c128b557602856d9c2862ee
Nina Loof <nina.loof@gmx.net>
2019-08-04 16:04:30 +0200

    Scores are displaying in Result Screen

--- Assets/Scripts/UI/MainUi.cs
+++ Assets/Scripts/UI/MainUi.cs
@@ -18,11 +18,16 @@ public class MainUi : MonoBehaviour
     private Text timerText;
     [SerializeField]
     private Image[] defectTerminalIcons;
+    [SerializeField]
+    private Image alarmOverlay;
     [Header("Colors")]
     [SerializeField]
     private Color defectTerminalIconDefaultColor;
     [SerializeField]
     private Color defectTerminalIconActiveColor;
+    [Header("Sounds")]
+    [SerializeField]
+    private AudioClip alarmSound;
     // Private
     private int numberOfDestroyedTerminals = 0;
     #endregion
@@ -79,6 +84,7 @@ public class MainUi : MonoBehaviour
     #region Private Functions
     private void UpdateDefectDisplay()
     {
+        // Update flame icons:
         for(int i = 0; i < defectTerminalIcons.Length; i++)
         {
             if(numberOfDestroyedTerminals > i)
@@ -86,6 +92,37 @@ public class MainUi : MonoBehaviour
             else
                 defectTerminalIcons[i].color = defectTerminalIconDefaultColor;
         }
+
+        // Toggle alarm overlay:
+        if (numberOfDestroyedTerminals >= 2)
+            ActivateAlarmOverlay();
+        else
+            DeactivateAlarmOverlay();
+    }
+
+    private void ActivateAlarmOverlay()
+    {
+        Debug.Log("ActivateAlarmOverlay");
+        if (!IsInvoking("ToogleAlarmOverlay"))
+        {
+            CancelInvoke("ToggleAlarmOverlay");
+            InvokeRepeating("ToggleAlarmOverlay", 1f, 1f);
+            alarmOverlay.enabled = true;
+        }
+    }
+
+    private void DeactivateAlarmOverlay()
+    {
+        Debug.Log("DeactivateAlarmOverlay");
+        CancelInvoke("ToggleAlarmOverlay");
+        alarmOverlay.enabled = false;
+    }
+
+    private void ToggleAlarmOverlay()
+    {
+        alarmOverlay.enabled = !alarmOverlay.enabled;
+        if (alarmOverlay.enabled)
+            SoundManager.PlaySound(alarmSound);
     }
     #endregion
 
