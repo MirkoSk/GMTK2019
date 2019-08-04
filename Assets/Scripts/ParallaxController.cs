42ad1e49eb75d3eeefeef97dc615a454916a2e0a
Friedemann <38406798+FSpitzner@users.noreply.github.com>
2019-08-04 11:13:38 +0200

    Parallax-Effect

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class ParallaxController : MonoBehaviour
+{
+
+    #region Variable Declarations
+    // Serialized Fields
+    [MinMaxRange(.25f, 4f)]
+    [SerializeField] RangedFloat movementSpeed;
+    [SerializeField] GameObject[] starPrefabs;
+    [MinMaxRange(.25f, 2f)]
+    [SerializeField] RangedFloat sizeModifier;
+    [MinMaxRange(0f, 2f)]
+    [SerializeField] RangedFloat spawnInterval;
+    [MinMaxRange(-10f, 10f)]
+    [SerializeField] RangedFloat spawnPositionYAxis;
+    [SerializeField] float spawnPositionXAxis;
+    [SerializeField] float destroyPositionXAxis;
+    [SerializeField] [SortingLayer] int sortingLayer;
+    // Private
+
+    #endregion
+
+
+
+    #region Public Properties
+
+    #endregion
+
+
+
+    #region Unity Event Functions
+    private void Start()
+    {
+        Invoke("SpawnParticle", Random.Range(spawnInterval.minValue, spawnInterval.maxValue));
+    }
+    #endregion
+
+
+
+    #region Public Functions
+
+    #endregion
+
+
+
+    #region Private Functions
+    private void SpawnParticle()
+    {
+        GameObject starGameObject = starPrefabs[Random.Range(0, starPrefabs.Length)];
+        float speed = Random.Range(movementSpeed.minValue, movementSpeed.maxValue);
+        float size = Random.Range(sizeModifier.minValue, sizeModifier.maxValue);
+        GameObject spawnedObject = Instantiate(starGameObject, new Vector3(spawnPositionXAxis, Random.Range(spawnPositionYAxis.minValue, spawnPositionYAxis.maxValue), 0f), Quaternion.identity, transform);
+        spawnedObject.transform.localScale = new Vector3(size, size, size);
+        Star star = spawnedObject.GetComponent<Star>();
+        SpriteRenderer starSprite = spawnedObject.GetComponent<SpriteRenderer>();
+        starSprite.sortingLayerID = sortingLayer;
+        star.Speed = speed;
+        star.DestroyPositionXAxis = destroyPositionXAxis;
+        Invoke("SpawnParticle", Random.Range(spawnInterval.minValue, spawnInterval.maxValue));
+    }
+    #endregion
+
+
+
+    #region Coroutines
+
+    #endregion
+}
+
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Star : MonoBehaviour 
+{
+
+    #region Variable Declarations
+    // Serialized Fields
+
+    // Private
+    private float speed = 0f;
+    private float destroyPositionXAxis = 0f;
+	#endregion
+	
+	
+	
+	#region Public Properties
+	public float Speed { set { speed = value; } }
+    public float DestroyPositionXAxis { set { destroyPositionXAxis = value;  } }
+    #endregion
+
+
+
+    #region Unity Event Functions
+    private void Update()
+    {
+        if (speed != 0 && destroyPositionXAxis != 0)
+        {
+            transform.position = new Vector3(transform.position.x - .1f * speed, transform.position.y, transform.position.z);
+            if (transform.position.x <= destroyPositionXAxis)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    #endregion
+
+
+
+    #region Public Functions
+
+    #endregion
+
+
+
+    #region Private Functions
+
+    #endregion
+
+
+
+    #region Coroutines
+
+    #endregion
+}
+
