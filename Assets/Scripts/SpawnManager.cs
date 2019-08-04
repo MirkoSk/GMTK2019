82d1276a76a446a70908227efeb9324462562348
Mirko <mskroch@uniglow-entertainment.com>
2019-08-04 20:10:30 +0200

    Balancing 2.0

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,17 +7,26 @@ using UnityEngine;
 /// </summary>
 public class SpawnManager : MonoBehaviour 
 {
+    [System.Serializable]
+    public class TerminalSpawns
+    {
+        public TerminalType TerminalType;
+        public int TimesToSpawn;
+    }
 
     #region Variable Declarations
     // Serialized Fields
     [Header("Terminal Settings")]
-    [SerializeField] List<Transform> terminalPositions;
-    [SerializeField] List<GameObject> terminalPrefabs;
     [Tooltip("Number of Terminals that get placed in the Level at game start")]
     [SerializeField] int numberOfTerminals;
+    [SerializeField] GameObject terminalPrefab;
+    [SerializeField] List<TerminalSpawns> terminalTypes = new List<TerminalSpawns>();
+    [SerializeField] List<Transform> terminalPositions = new List<Transform>();
+    [SerializeField] TerminalType fillerTerminal = null;
+    
 
-    [Header("MiniGames")]
-    [SerializeField] List<GameObject> miniGamePrefabs;
+    [Header("Minigames")]
+    [SerializeField] List<GameObject> miniGamePrefabs = new List<GameObject>();
     [SerializeField] GameObject repairMinigamePrefab;
 
     // Private
@@ -50,19 +59,27 @@ public class SpawnManager : MonoBehaviour
 	#region Private Functions
     void SpawnTerminals()
     {
+        List<TerminalSpawns> availableTerminals = terminalTypes;
+
         for (int i = 0; i < numberOfTerminals; i++)
         {
-            int prefabNumber = Random.Range(0, terminalPrefabs.Count);
             int parentNumber = Random.Range(0, terminalPositions.Count);
             Transform parent = terminalPositions[parentNumber];
 
+            int terminalTypeNumber = Random.Range(0, availableTerminals.Count);
+            TerminalType terminalType = null;
+            if (availableTerminals.Count > 0)
+                terminalType = availableTerminals[terminalTypeNumber].TerminalType;
+            else
+                terminalType = fillerTerminal;
+
             //Spawn Terminal
             parent.gameObject.SetActive(true);
-            GameObject terminalPrefab = Instantiate(terminalPrefabs[prefabNumber], parent.position, parent.rotation, parent);
-            TerminalController terminalController = terminalPrefab.GetComponent<TerminalController>();
+            GameObject terminal = Instantiate(terminalPrefab, parent.position, parent.rotation, parent);
+            TerminalController terminalController = terminal.GetComponent<TerminalController>();
 
             //Spawn Minigame
-            GameObject minigame = Instantiate(miniGamePrefabs[Random.Range(0, miniGamePrefabs.Count)], terminalController.MinigamePosition);
+            GameObject minigame = Instantiate(terminalType.Minigame, terminalController.MinigamePosition);
             Minigame minigameController = minigame.GetComponent<Minigame>();
 
             //Spawn RepairMinigame
@@ -73,8 +90,15 @@ public class SpawnManager : MonoBehaviour
             terminalController.LinkedMinigame = minigameController;
             terminalController.RepairMinigame = repairMinigameController;
 
+            // Set TimeToFail
+            terminalController.TimeToFail = terminalType.TimeToFail;
+
             terminals.Add(terminalController);
+
+            // Cleanup
             terminalPositions.RemoveAt(parentNumber);
+            availableTerminals[terminalTypeNumber].TimesToSpawn--;
+            if (availableTerminals[terminalTypeNumber].TimesToSpawn <= 0) availableTerminals.RemoveAt(terminalTypeNumber);
         }
     }
 	#endregion
